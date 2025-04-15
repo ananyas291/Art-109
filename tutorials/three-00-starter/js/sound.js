af710131c2b48f5cbee94ba101134d4c6ac73357
ananyas291 <168224898+ananyas291@users.noreply.github.com>
2025-04-15 14:14:37 -0700

    d

--- tutorials/three-00-starter/js/sound.js
+++ tutorials/three-00-starter/js/sound.js
@@ -1,2 +1,11 @@
-let song = document.querySelector("#song");
-song.play();
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const song = document.getElementById('song');
+
+playButton.addEventListener('click', () => {
+  song.play();
+});
+
+pauseButton.addEventListener('click', () => {
+  song.pause();
+});
